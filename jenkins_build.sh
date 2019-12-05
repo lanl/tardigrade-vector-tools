49f3152f619db80105d44ead40ca627b1f70dc24
Kyle Andrew Brindley <kbrindley@lanl.gov>
2019-12-05 13:27:20 -0700

    FEAT: first draft bash test script; should consider moving dependency location checking and cloning into a make file so we don't duplicate dependency trees

---
+++ jenkins_build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+workdir=${PWD}
+eigendir='eigen'
+eigenrepo='https://gitlab.com/libeigen/eigen.git'
+
+# Make bash script more like high-level languages.
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -Eeuxo pipefail
+
+cd ..
+if [ ! -d ${eigendir} ]; then
+    git clone ${eigenrepo}
+else
+    cd ${eigendir} && git pull
+fi
+cd ${workdir}
+cd src/cpp/test/vector_tools/
+make
