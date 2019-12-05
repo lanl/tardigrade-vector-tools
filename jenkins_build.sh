8df5ee885570b06d6c33e7f22fac4c384208bf9b
Kyle Andrew Brindley <kbrindley@lanl.gov>
2019-12-05 15:03:23 -0700

    BUG: need to add proxy out to every git operation

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -3,6 +3,7 @@
 workdir=${PWD}
 eigendir='eigen'
 eigenrepo='https://gitlab.com/libeigen/eigen.git'
+proxyout='proxyout.lanl.gov:8080'
 
 # Make bash script more like high-level languages.
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
@@ -10,9 +11,9 @@ set -Eeuxo pipefail
 
 cd ..
 if [ ! -d ${eigendir} ]; then
-    all_proxy=proxyout.lanl.gov:8080 git clone ${eigenrepo}
+    all_proxy=${proxyout} git clone ${eigenrepo}
 else
-    cd ${eigendir} && git pull
+    cd ${eigendir} && all_proxy=${proxyout} git pull
 fi
 cd ${workdir}
 cd src/cpp/test/vector_tools/
