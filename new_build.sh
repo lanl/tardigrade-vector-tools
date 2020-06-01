6293d71cec0db462e51e208a6da83c0b59443c5b
Kyle Andrew Brindley <kbrindley@lanl.gov>
2020-06-01 15:48:02 -0600

    FEAT: add build script for convenience

---
+++ new_build.sh
@@ -0,0 +1,18 @@
+set -Eeuxo pipefail
+
+# Find cmake3 executable
+if [ -x "$(command -v cmake3)" ]; then
+    cmake_exec=$(command -v cmake3)
+elif [ -x "$(command -v cmake)" ]; then
+    cmake_exec=$(command -v cmake)
+else
+    echo "Could not find cmake executable"
+    exit 1
+fi
+
+# Clean and build repo tests
+rm -rf build/
+mkdir build
+cd build
+${cmake_exec} ..
+${cmake_exec} --build .
