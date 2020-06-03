ffe98f168c9a50fba60fe39946f16ade47346944
Kyle Andrew Brindley <kbrindley@lanl.gov>
2020-06-03 10:15:05 -0600

    BUG: more debugging output in build scripts

--- new_build.sh
+++ new_build.sh
@@ -6,7 +6,17 @@ case $(hostname) in
         ;;
 esac
 
+# Make bash script more like high-level languages.
 set -Eeuxo pipefail
+
+# Debugging
+whoami
+groups
+ls -l ~/include || true
+ls -l ~/include/eigen3 || true
+ls -l ~/include/eigen3/Eigen || true
+ls -l ~/include/eigen3/Eigen/Dense || true
+
 
 # Find cmake3 executable
 if [ -x "$(command -v cmake3)" ]; then
