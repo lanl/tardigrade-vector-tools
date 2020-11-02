d2c542e7ebacb1fae098aeb8041402106807ca65
Kyle Brindley <kbrindley@lanl.gov>
2020-11-02 16:27:36 -0700

    FEAT: add updated new_build shell script to allow different compilers

--- new_build.sh
+++ new_build.sh
@@ -1,13 +1,34 @@
+# USAGE:
+#
+# ./new_build.sh CXX_compiler
+
 # Make bash script more like high-level languages.
 set -Eeuxo pipefail
+
+# Get this scripts file name
+script=`basename "$0"`
+
+# Parse arguments
+if [ "$#" -ne 1 ]; then
+    echo "${script} USAGE:"
+    echo "./${script} CXX_compiler"
+    echo "    CXX_compiler: desired c++ compiler"
+    exit 1
+fi
+cxx_path=$1  # Path to CXX compiler
+
+# Verify compiler command and set CXX env variable
+if [ -x "$(command -v ${cxx_path})" ]; then
+    export CXX="$(command -v ${cxx_path})"
+else
+    exit 2
+fi
 
 # Debugging
 whoami
 groups
-ls -l ~/include || true
-ls -l ~/include/eigen3 || true
-ls -l ~/include/eigen3/Eigen || true
-ls -l ~/include/eigen3/Eigen/Dense || true
+ls -l $HOME/include || true
+ls -l $HOME/.local/include || true
 
 # Find cmake3 executable
 if [ -x "$(command -v cmake3)" ]; then
@@ -16,7 +37,7 @@ elif [ -x "$(command -v cmake)" ]; then
     cmake_exec=$(command -v cmake)
 else
     echo "Could not find cmake executable"
-    exit 1
+    exit 3
 fi
 
 # Clean and build repo tests
