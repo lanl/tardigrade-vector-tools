1b8141e82ef8cd465b344ce31e480780bf9f926b
Nathan Andrew Miller <nathanm@lanl.gov>
2023-05-05 16:00:34 -0600

    WIP: Added derivative of the inverse of a matrix w.r.t. the matrix

--- src/cpp/vector_tools.h
+++ src/cpp/vector_tools.h
@@ -300,6 +300,9 @@ namespace vectorTools{
         std::vector< std::vector< double > > inverse( const std::vector< std::vector< T > > &A );
 
         template<typename T>
+        std::vector< std::vector< double > > computeDAInvDA( const std::vector< T > &invA, const unsigned int nrows, const unsigned int ncols );
+
+        template<typename T>
         std::vector< double > computeDDetAdJ(const std::vector< T > &Avec, const unsigned int nrows, const unsigned int ncols);
 
         template< typename T >
